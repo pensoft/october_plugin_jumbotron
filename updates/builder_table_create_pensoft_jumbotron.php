aca7c8e2fed9622d91b39a3831f27db93d55926a
root <root@project-dev.pensoft.local>
2020-12-21 13:05:22 +0200

    update updates/builder_table_create_pensoft_jumbotron.php

--- updates/builder_table_create_pensoft_jumbotron.php
+++ updates/builder_table_create_pensoft_jumbotron.php
@@ -7,23 +7,27 @@ class BuilderTableCreatePensoftJumbotron extends Migration
 {
     public function up()
     {
-        Schema::create('pensoft_jumbotron', function($table)
-        {
-            $table->engine = 'InnoDB';
-            $table->increments('id')->unsigned();
-            $table->string('title');
-            $table->string('slug');
-            $table->text('body');
-            $table->string('button_name');
-            $table->string('url');
-            $table->string('url_target');
-            $table->timestamp('created_at')->nullable();
-            $table->timestamp('updated_at')->nullable();
-        });
+        if (!Schema::hasTable('pensoft_jumbotron')) {
+            Schema::create('pensoft_jumbotron', function($table)
+            {
+                $table->engine = 'InnoDB';
+                $table->increments('id')->unsigned();
+                $table->string('title');
+                $table->string('slug');
+                $table->text('body');
+                $table->string('button_name');
+                $table->string('url');
+                $table->string('url_target');
+                $table->timestamp('created_at')->nullable();
+                $table->timestamp('updated_at')->nullable();
+            });
+        }
     }
     
     public function down()
     {
-        Schema::dropIfExists('pensoft_jumbotron');
+        if (Schema::hasTable('pensoft_jumbotron')) {
+            Schema::dropIfExists('pensoft_jumbotron');
+        }
     }
 }
